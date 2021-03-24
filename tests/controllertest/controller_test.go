51d8864cf45f361357c683cdb0ae39b356470214
istiyawan <istiyawan@gmail.com>
2021-03-24 08:52:16 +0700

    add unit testing

---
+++ tests/controllertest/controller_test.go
@@ -0,0 +1,13 @@
+package controllertests
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/istiyawan/marketpasarjwt/api/controllers"
+	"github.com/istiyawan/marketpasarjwt/api/models"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+)
